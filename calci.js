8353736fd683cfaa742e782e9e25b5c1a768b0c3
Aman <aman.d6t@gmail.com>
2024-05-24 15:51:26 +0530

    initial commit

---
+++ calci.js
@@ -0,0 +1,36 @@
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const subtract = (a, b) => {
+//   return a - b;
+// };
+
+// const multiply = (a, b) => {
+//   return a * b;
+// };
+
+// const divide = (a, b) => {
+//   return a / b;
+// };
+
+// const cObj = {
+//   add,
+//   subtract,
+//   multiply,
+//   divide,
+// };
+
+// module.exports = cObj;
+
+const obj = {
+  a: "Original Value",
+  b: 6,
+};
+console.log(obj.a);
+
+obj.a = "New Value";
+console.log(obj.a);
+
+obj.b = b + 10;
+console.log(obj.b);
---
+++ first.js
@@ -0,0 +1,6 @@
+const { add, subtract, multiply, divide } = require("./calci.js");
+
+console.log(add(4, 5));
+console.log(subtract(4, 5));
+console.log(multiply(4, 5));
+console.log(divide(4, 5));
