f105314e499c1420f17b7ecefd25fca1f1fc4096
Aman <aman.d6t@gmail.com>
2024-06-05 22:47:45 +0530

    commit

--- first.js
+++ first.js
@@ -1,5 +1,10 @@
 // const { add, subtract, multiply, divide } = require("./calci.js");
 
+// console.log(add(4, 5));
+// console.log(subtract(4, 5));
+// console.log(multiply(4, 5));
+// console.log(divide(4, 5));
+
 // console.log(process.argv);
 
 // console.log("Hello " + process.argv[2]);
